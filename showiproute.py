6b54c682da7a07b8b0c81c2461f6407f54bf2ae0
wbculver <wbculver@gmail.com>
2023-08-11 16:03:12 -0400

    Update showiproute.py

--- showiproute.py
+++ showiproute.py
@@ -1,9 +1,9 @@
 from netmiko import ConnectHandler
 import re
-import pandas as pd
+import csv
 
-# Function to retrieve "show ip route" for a device
-def get_ip_route(device_ip, username, password, enable_password):
+# Function to retrieve "show ip route" for a device and remove timestamps
+def get_ip_route_without_timestamps(device_ip, username, password, enable_password):
     # Cisco device information
     device = {
         "device_type": "cisco_ios",  # Use the appropriate device type for your ASR
@@ -34,21 +34,28 @@ def get_ip_route(device_ip, username, password, enable_password):
     return ip_route_output
 
 # List of device IP addresses to download "show ip route" from
-device_ips = ["192.168.1.1", "192.168.1.2"]  # Add more IP addresses as needed
+device_ips = ["YOUR_DEVICE_IP"]  # Replace with the actual IP addresses
 
-# Create a dictionary to store the "show ip route" output for each device
-ip_route_data = {}
+# Define your credentials
+username = "YOUR_USERNAME"
+password = "YOUR_PASSWORD"
+enable_password = "YOUR_ENABLE_PASSWORD"
 
-# Retrieve "show ip route" for each device
+# Create a list to store the "show ip route" output for each device
+ip_route_data = []
+
+# Retrieve "show ip route" without timestamps for each device
 for ip in device_ips:
-    ip_route_output = get_ip_route(ip, "YOUR_USERNAME", "YOUR_PASSWORD", "YOUR_ENABLE_PASSWORD")
-    ip_route_data[ip] = ip_route_output
+    ip_route_output = get_ip_route_without_timestamps(ip, username, password, enable_password)
+    ip_route_data.append(ip_route_output)
 
-# Create a Pandas DataFrame from the dictionary
-df = pd.DataFrame(ip_route_data.items(), columns=["Device IP", "Show IP Route"])
+# Save the IP route data to a CSV file
+output_file = "show_ip_route_output.csv"
+with open(output_file, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    for route_output in ip_route_data:
+        # Split the route output into lines and write each line as a separate row
+        for line in route_output.split('\n'):
+            csv_writer.writerow([line])
 
-# Save the DataFrame to an Excel file
-output_file = "show_ip_route_output.xlsx"
-df.to_excel(output_file, index=False)
-
-print(f"Show IP Route data saved to {output_file}")
+print(f"Show IP Route data (without timestamps) saved to {output_file}")
