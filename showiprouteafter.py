0989c896c8b734cd94197d6e190c2f2ead40eafe
wbculver <wbculver@gmail.com>
2023-08-11 23:59:19 -0400

    Update showiprouteafter.py

--- showiprouteafter.py
+++ showiprouteafter.py
@@ -51,63 +51,36 @@ def get_ip_route_without_timestamps(device_ip):
         print(f"Error connecting to {device_ip}: {str(e)}")
         return ""
 
-# Function to save IP route data to an Excel file
-def save_ip_route_to_excel(data, output_file):
-    # Create a Pandas DataFrame
-    df = pd.DataFrame({"Route Data": data})
-    # Create a Pandas Excel writer using XlsxWriter as the engine
-    with pd.ExcelWriter(output_file, engine='xlsxwriter') as writer:
-        # Write the DataFrame to a worksheet
-        df.to_excel(writer, sheet_name="All Routes", index=False)
-
 # Retrieve "show ip route" without timestamps for each device (AFTER) with tqdm
 ip_route_data_after = {}
 for ip in tqdm(device_ips, desc="Retrieving routes"):
     route_output_after = get_ip_route_without_timestamps(ip)
     ip_route_data_after[ip] = route_output_after
 
-# Save all routes in "All Routes" sheet
-output_file_all = "EquinixAllRoutes.xlsx"
-all_routes = {}
-for ip, route_data in ip_route_data_after.items():
-    all_routes[ip] = route_data.split("\n")
+# Save all routes in separate sheets within the same Excel file
+output_file = "EquinixRoutesComparison.xlsx"
+with pd.ExcelWriter(output_file, engine='xlsxwriter') as writer:
+    # Save the initial routes in the first sheet
+    for ip in device_ips:
+        df_initial = pd.DataFrame({"Route Data": ip_route_data_after[ip].split("\n")})
+        df_initial.to_excel(writer, sheet_name=f"Initial - {ip}", index=False)
 
-save_ip_route_to_excel(all_routes, output_file_all)
-print(f"All routes saved to {output_file_all}")
+    # Compare the routes before and after
+    for ip in tqdm(device_ips, desc="Comparing routes"):
+        df_changes = pd.DataFrame(columns=["Change Type", "Before", "After"])
 
-# Load the initial route data from the "before" Excel file
-initial_file = "EquinixRoutesBeforeChange.xlsx"
-df_initial = pd.read_excel(initial_file, sheet_name=None)
+        # Filter the initial data for the current device IP
+        initial_routes = df_initial["Route Data"].astype(str).tolist()
 
-# Compare the routes before and after
-changes = {}
-for ip in tqdm(device_ips, desc="Comparing routes"):
-    changes[ip] = []
+        # Retrieve the route data after the change
+        route_after = ip_route_data_after.get(ip, "").split("\n")
 
-    # Filter the initial data for the current device IP
-    initial_routes = df_initial[ip]["Route Data"].astype(str).tolist()
+        # Find changes
+        for route_before, route_after in zip(initial_routes, route_after):
+            if route_before.strip() != route_after.strip():
+                df_changes = df_changes.append({"Change Type": "Route Data", "Before": route_before.strip(), "After": route_after.strip()}, ignore_index=True)
 
-    # Retrieve the route data after the change
-    route_after = ip_route_data_after.get(ip, "").split("\n")
+        # Save changes to a separate sheet
+        df_changes.to_excel(writer, sheet_name=f"Changes - {ip}", index=False)
 
-    # Find changes
-    for route_before, route_after in zip(initial_routes, route_after):
-        if route_before.strip() != route_after.strip():
-            changes[ip].append(("Route Data", route_before.strip(), route_after.strip()))
-
-# Save the changes to "EquinixRoutesAfterChange.xlsx"
-output_file_changed = "EquinixRoutesAfterChange.xlsx"
-with pd.ExcelWriter(output_file_changed, engine='xlsxwriter') as writer:
-    for ip, change_data in changes.items():
-        if change_data:
-            # Create a DataFrame with the change information
-            df_changes = pd.DataFrame(change_data, columns=["Change Type", "Before", "After"])
-            # Write the DataFrame to a worksheet named after the IP address
-            df_changes.to_excel(writer, sheet_name=ip, index=False)
-            worksheet = writer.sheets[ip]
-            # Adjust the column widths
-            worksheet.set_column('A:A', 15)
-            worksheet.set_column('B:B', 60)
-            worksheet.set_column('C:C', 60)
-
-print(f"Show IP Route data changes saved to {output_file_changed}")
+print(f"Show IP Route data comparison saved
