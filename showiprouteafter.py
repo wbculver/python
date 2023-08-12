d0e8dc1c473b2976b4ab4597d0d48394e7f99524
wbculver <wbculver@gmail.com>
2023-08-12 04:32:04 -0400

    Update showiprouteafter.py

--- showiprouteafter.py
+++ showiprouteafter.py
@@ -1,60 +1,88 @@
-import pandas as pd
-import re
 from netmiko import ConnectHandler
-import traceback
+import re
+import pandas as pd
 from tqdm import tqdm
 
-def get_ip_route_without_timestamps(device_ip, username, password):
-    # The implementation of this function remains the same as in your original script
-    # ...
+# Variables
+username = input("Enter your username: ")
+password = input("Enter your password: ")
 
-if __name__ == "__main__":
-    # Input file for the IP route data before the change
-    input_file_before = "EquinixRoutesBeforeChange.xlsx"
+# List of device IP addresses to compare "show ip route" with the previous script output
+device_ips = [
+    "10.111.237.200",
+    "10.111.237.201",
+]
 
-    # Prompt for the username and password
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
+# Load the previous script output
+previous_output_file = "EquinixRoutesAfterChange.xlsx"
+previous_ip_route_data = {}
 
-    # Define device IP addresses to download "show ip route" from
-    device_ips = [
-        "10.111.237.200",
-    ]
+xls = pd.ExcelFile(previous_output_file)
+for sheet_name in xls.sheet_names:
+    df = pd.read_excel(xls, sheet_name=sheet_name)
+    previous_ip_route_data[sheet_name] = df["Route Data"].tolist()
 
-    # Create a Pandas DataFrame for each worksheet in the Excel file (before change)
-    dfs_before = []
-    for sheet_name in pd.ExcelFile(input_file_before).sheet_names:
-        df = pd.read_excel(input_file_before, sheet_name=sheet_name, dtype=str)
-        dfs_before.append(df)
+# Function to retrieve "show ip route" for a device and remove timestamps
+def get_ip_route_without_timestamps(device_ip):
+    # Cisco device information
+    device = {
+        "device_type": "cisco_ios",
+        "ip": device_ip,
+        "username": username,
+        "password": password,
+        "timeout": 60,  # Increase the timeout if needed
+        "global_delay_factor": 2,  # Add a delay factor to allow more time for the command output
+    }
 
-    # Create a dictionary to store the differences between the two sets of IP route data
-    differences = {}
+    # Connect to the device
+    net_connect = ConnectHandler(**device)
 
-    # Use tqdm to visualize the progress
-    for device_ip in tqdm(device_ips, desc="Processing devices"):
-        try:
-            ip_route_data[device_ip] = get_ip_route_without_timestamps(device_ip, username, password)
+    try:
+        # Send the "terminal length 0" command
+        net_connect.send_command_timing("terminal length 0")
 
-            # Compare the IP route data for this device
-            df_before = dfs_before[device_ips.index(device_ip)]  # Get the corresponding DataFrame (before change)
-            route_entries_before = [re.sub(r"(\d{1,2}:\d{1,2}:\d{1,2}|\d{1,4}w\d{1,2}d)\s", "", entry) for entry in df_before["Route Data"].tolist()]
+        # Send the "show ip route" command and retrieve the output
+        output = net_connect.send_command_timing("show ip route")
 
-            # ... rest of the comparison code ...
-            
-        except Exception as e:
-            print(f"An error occurred while processing {device_ip}. {str(e)}")
-            traceback.print_exc()  # Print the full traceback for better error analysis
+        # Define regex patterns to match timestamps (e.g., 00:00:00 or 12:34:56)
+        # and relative time format (e.g., 5w1d, 1d5h)
+        timestamp_pattern = re.compile(r'\d{2}:\d{2}:\d{2}')
+        relative_time_pattern = re.compile(r'\d+[wdhms]')
+        
+        # Remove timestamps and relative time format from each line
+        ip_route_output = "\n".join([
+            relative_time_pattern.sub('', timestamp_pattern.sub('', line))
+            for line in output.split("\n")
+        ])
 
-    # Save the differences to a separate sheet in the "before change" Excel file
-    output_file = "EquinixRoutesComparisonOutput.xlsx"
-    with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
-        # Write the original IP route data to a sheet
-        for device_ip, df in zip(device_ips, dfs_before):
-            df.to_excel(writer, sheet_name=f"Device_{device_ip}", index=False)
+        return ip_route_output
+    finally:
+        # Disconnect from the device
+        net_connect.disconnect()
 
-        # Write the comparison results to a separate sheet
-        for device_ip, diff_list in differences.items():
-            diff_df = pd.DataFrame(diff_list, columns=["Index", "Old Route"])
-            diff_df.to_excel(writer, sheet_name=f"Differences_{device_ip}", index=False)
+# Create a dictionary to store the current "show ip route" output for each device
+current_ip_route_data = {}
 
-    print(f"Differences in IP routes comparison saved to {output_file}")
+# Retrieve current "show ip route" without timestamps for each device
+for ip in tqdm(device_ips, desc="Retrieving current IP routes"):
+    current_ip_route_data[ip] = get_ip_route_without_timestamps(ip)
+
+# Compare the current routes with the previous output
+for ip, current_route_output in current_ip_route_data.items():
+    sheet_name = re.sub(r'[\/:*?"<>|]', '_', ip)
+    previous_routes = previous_ip_route_data.get(sheet_name, [])
+    current_routes = current_route_output.split("\n")
+    
+    added_routes = [route for route in current_routes if route not in previous_routes]
+    removed_routes = [route for route in previous_routes if route not in current_routes]
+    unchanged_routes = [route for route in current_routes if route in previous_routes]
+    
+    print(f"Device IP: {ip} - Route Comparison Summary")
+    print(f"Routes Added: {len(added_routes)}")
+    for route in added_routes:
+        print(f"  + {route}")
+    print(f"Routes Removed: {len(removed_routes)}")
+    for route in removed_routes:
+        print(f"  - {route}")
+    print(f"Routes Unchanged: {len(unchanged_routes)}")
+    print(f"--------------------------------------------")
