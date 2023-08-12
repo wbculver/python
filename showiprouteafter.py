3bb9229e5d2d16a4482dccdf92be5cff6cf57e46
wbculver <wbculver@gmail.com>
2023-08-12 04:08:01 -0400

    Update showiprouteafter.py

--- showiprouteafter.py
+++ showiprouteafter.py
@@ -61,33 +61,35 @@ for device_ip in device_ips:
 
         # Compare the IP route data for this device
         df_before = dfs_before[device_ip]  # Get the corresponding DataFrame (before change)
-        route_entries_before = df_before["Route Data"].tolist()
+        route_entries_before = set(df_before["Route Data"].tolist())
         route_output = ip_route_data[device_ip]
-        route_entries_after = route_output.split("\n")
+        route_entries_after = set(route_output.split("\n"))
 
-        for index, route_entry in enumerate(route_entries_before):
-            if route_entry not in route_entries_after:
-                if device_ip not in differences:
-                    differences[device_ip] = []
-                differences[device_ip].append({
-                    "index": index,
-                    "old_route": route_entry,
-                })
+        # Find the differences between the two sets of route entries
+        diff_entries = route_entries_before.symmetric_difference(route_entries_after)
+
+        # Store the differences
+        if diff_entries:
+            differences[device_ip] = diff_entries
     except Exception as e:
         print(f"An error occurred while processing {device_ip}. {str(e)}")
         traceback.print_exc()  # Print the full traceback for better error analysis
 
-# Save the differences to an Excel file with separate sheets for routes that are different
+# Save the "before" and "after" route data, and differences to an Excel file
 output_file = "EquinixRoutesComparisonOutput.xlsx"
 with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
-    # Write the original IP route data to separate sheets
+    # Write the original IP route data (before change) to separate sheets
     for device_ip, df in dfs_before.items():
-        df.to_excel(writer, sheet_name=f"Device_{device_ip}", index=False)
+        df.to_excel(writer, sheet_name=f"Before_Device_{device_ip}", index=False)
 
-    # Write the comparison results to a separate sheet for routes that are different
-    for device_ip, diff_list in differences.items():
-        if diff_list:
-            diff_df = pd.DataFrame(diff_list, columns=["Index", "Old Route"])
-            diff_df.to_excel(writer, sheet_name=f"Differences_{device_ip}", index=False)
+    # Write the current IP route data (after change) to separate sheets
+    for device_ip, route_output in ip_route_data.items():
+        df = pd.DataFrame({"Route Data": route_output.split("\n")})
+        df.to_excel(writer, sheet_name=f"After_Device_{device_ip}", index=False)
+
+    # Write the differences to a separate sheet for each device
+    for device_ip, diff_entries in differences.items():
+        df_diff = pd.DataFrame({"Different Route Data": list(diff_entries)})
+        df_diff.to_excel(writer, sheet_name=f"Differences_Device_{device_ip}", index=False)
 
 print(f"Differences in IP routes comparison saved to {output_file}")
