ac33555e088e7e8d2360e77775c52d59cb532794
wbculver <wbculver@gmail.com>
2023-08-24 22:40:54 -0400

    Update pythonconfig-new-diff.py

--- pythonconfig-new-diff.py
+++ pythonconfig-new-diff.py
@@ -1,5 +1,4 @@
 from netmiko import ConnectHandler
-import yaml
 from tqdm import tqdm
 import difflib
 
@@ -9,28 +8,30 @@ device_username = "admin"
 device_password = "admin"
 device_type = "cisco_ios"
 
-# Load intended configuration changes from YAML file
-yaml_file = "config.yaml"
-with open(yaml_file) as f:
-    config_data = yaml.safe_load(f)
-
-# Extract intended changes from the YAML file
-intended_changes = config_data.get("config_changes", [])
+# Read the full running configuration from a text file
+with open("full_running_config.txt", "r") as f:
+    full_running_config = f.read()
 
 # Read the last applied change from the text file
 last_change_file = "last_change.txt"
 with open(last_change_file, "r") as f:
     last_applied_change = f.read()
 
-# Find the index of the last applied change in intended_changes
-last_change_index = -1
-for idx, change in enumerate(intended_changes):
-    if change.strip() == last_applied_change.strip():
-        last_change_index = idx
-        break
+# Find the position of the last applied change in the full running configuration
+last_change_position = full_running_config.find(last_applied_change)
 
-# Extract new intended changes to compare
-new_changes_to_apply = intended_changes[last_change_index + 1:]
+# Extract the portion of the full running config after the last applied change
+running_config_after_last_change = full_running_config[last_change_position + len(last_applied_change):]
+
+# Read the new changes to apply from a separate text file
+with open("new_changes.txt", "r") as f:
+    new_changes_to_apply = f.read()
+
+# Use difflib to compare and highlight differences
+diff = difflib.unified_diff(running_config_after_last_change.splitlines(), new_changes_to_apply.splitlines(), lineterm='')
+
+# Check if there are any differences
+differences_exist = any(line.startswith('+ ') for line in diff)
 
 # Connect to the device
 with ConnectHandler(**{
@@ -42,24 +43,6 @@ with ConnectHandler(**{
     "timeout": 300,
     "global_cmd_verify": False,
 }) as net_connect:
-    # Download the entire running configuration
-    running_config = net_connect.send_command("show running-config")
-    
-    # Find the position of the last applied change in the running configuration
-    last_change_position = running_config.find(last_applied_change)
-
-    # Extract the portion of the running config after the last applied change
-    running_config_after_last_change = running_config[last_change_position + len(last_applied_change):]
-
-    # Normalize the new intended changes for comparison
-    new_changes_normalized = "\n".join(line.strip() for line in new_changes_to_apply)
-
-    # Use difflib to compare and highlight differences
-    diff = difflib.unified_diff(running_config_after_last_change.splitlines(), new_changes_normalized.splitlines(), lineterm='')
-
-    # Check if there are any differences
-    differences_exist = any(line.startswith('+ ') for line in diff)
-
     # If there are differences, apply the changes
     if differences_exist:
         print("New configuration changes found, applying changes...")
@@ -79,7 +62,7 @@ with ConnectHandler(**{
         # Apply the changes
         config_commands = [
             "configure terminal",
-            "\n".join(new_changes_to_apply),
+            "\n".join(new_changes_to_apply.splitlines()),
             "end"
         ]
 
@@ -90,7 +73,7 @@ with ConnectHandler(**{
 
         # Update the last applied change in the text file
         with open(last_change_file, "w") as f:
-            f.write("\n".join(new_changes_to_apply))
+            f.write(new_changes_to_apply)
     else:
         print("No new configuration changes needed.")
 
