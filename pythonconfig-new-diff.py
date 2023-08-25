a31132d57be456cf64ce76b56868b69234801848
wbculver <wbculver@gmail.com>
2023-08-24 22:59:21 -0400

    Update pythonconfig-new-diff.py

--- pythonconfig-new-diff.py
+++ pythonconfig-new-diff.py
@@ -14,23 +14,6 @@ last_change_file = "last_change.txt"
 with open(last_change_file, "r") as f:
     last_applied_change = f.read()
 
-# Find the position of the last applied change in the running configuration
-with ConnectHandler(**{
-    "ip": device_ip,
-    "username": device_username,
-    "password": device_password,
-    "device_type": device_type,
-    "global_delay_factor": 3,
-    "timeout": 300,
-    "global_cmd_verify": False,
-}) as net_connect:
-    running_config = net_connect.send_command("show running-config")
-
-last_change_position = running_config.find(last_applied_change)
-
-# Extract the portion of the running config after the last applied change
-running_config_after_last_change = running_config[last_change_position + len(last_applied_change):]
-
 # Get the modification timestamp of the new_changes.txt file
 new_changes_file_path = "new_changes.txt"
 new_changes_timestamp = os.path.getmtime(new_changes_file_path)
@@ -53,6 +36,14 @@ with ConnectHandler(**{
 }) as net_connect:
     # If the new_changes.txt file has been modified, read and apply changes
     if new_changes_modified:
+        # Fetch the current running configuration
+        running_config = net_connect.send_command("show running-config")
+        
+        last_change_position = running_config.find(last_applied_change)
+
+        # Extract the portion of the running config after the last applied change
+        running_config_after_last_change = running_config[last_change_position + len(last_applied_change):]
+
         with open(new_changes_file_path, "r") as f:
             new_changes_to_apply = f.read()
         
