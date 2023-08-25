2540242f2b68ce31e9d9359e99eca11e32d782ba
wbculver <wbculver@gmail.com>
2023-08-24 21:07:09 -0400

    Update pythonconfig-new-diff.py

--- pythonconfig-new-diff.py
+++ pythonconfig-new-diff.py
@@ -1,50 +1,52 @@
-
 from netmiko import ConnectHandler
 import yaml
 from tqdm import tqdm
-import socket
 
+# Device information
 device_ip = "10.111.237.162"
 device_username = "admin"
 device_password = "admin"
 device_type = "cisco_ios"
+
+# Load configuration changes from YAML file
 yaml_file = "config.yaml"
-with open(yaml_file) as f: config_changes = yaml.safe_load(f)
+with open(yaml_file) as f:
+    config_changes = yaml.safe_load(f)
+
+# Connect to the device
 with ConnectHandler(**{
     "ip": device_ip,
     "username": device_username,
     "password": device_password,
     "device_type": device_type,
     "global_delay_factor": 3,
-    "timeout":  300,
+    "timeout": 300,
     "global_cmd_verify": False,
+}) as net_connect:
+    running_config = net_connect.send_command("show running-config")
 
-}) as net_connect: running_config = net_connect.send_command("show running-config")
+    # Loop through each change in config_changes
+    for change in tqdm(config_changes, desc="Applying Configuration Changes", unit="interface"):
+        interface = change.get("interface")
+        descriptions = change.get("descriptions", [])
+        
+        config_snippet = f"interface {interface}\n"
+        config_snippet += '\n'.join(f"  description {description}" for description in descriptions)
 
-for change in tqdm(config_changes, desc="Applying Configuration Changes", unit="interface"): interface = change.get("interface")
-descriptions = change.get("descriptions", [])
-
-config_snippet = f"interface {interface}\n"
-
-config_snippet += '\n'.join(f"  description {description}"
-for description
-in descriptions)
-
-if config_snippet.strip() in running_config:
-  print (f"No configuration changes needed for {interface}.")
-else:
-  print (f"Configuration differs for {interface}, applying changes...")
-
-config_commands = [
-"configure terminal",
+        if config_snippet.strip() in running_config:
+            print(f"No configuration changes needed for {interface}.")
+        else:
+            print(f"Configuration differs for {interface}, applying changes...")
+            
+            config_commands = [
+                "configure terminal",
                 config_snippet,
                 "end"
             ]
 
-output = "" 
-for  cmd in config_commands:
-  output += net_connect.send_command_timing(cmd
-+ "\n") 
-  print(output)
+            output = ""
+            for cmd in config_commands:
+                output += net_connect.send_command_timing(cmd + "\n")
+                print(output)
 
 print("Configuration update completed.")
