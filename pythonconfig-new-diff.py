a105ae9849a5245435da7576eff555f9371ffd51
wbculver <wbculver@gmail.com>
2023-08-24 22:53:03 -0400

    Update pythonconfig-new-diff.py

--- pythonconfig-new-diff.py
+++ pythonconfig-new-diff.py
@@ -14,14 +14,22 @@ last_change_file = "last_change.txt"
 with open(last_change_file, "r") as f:
     last_applied_change = f.read()
 
-# Find the position of the last applied change in the full running configuration
-with open("full_running_config.txt", "r") as f:
-    full_running_config = f.read()
+# Find the position of the last applied change in the running configuration
+with ConnectHandler(**{
+    "ip": device_ip,
+    "username": device_username,
+    "password": device_password,
+    "device_type": device_type,
+    "global_delay_factor": 3,
+    "timeout": 300,
+    "global_cmd_verify": False,
+}) as net_connect:
+    running_config = net_connect.send_command("show running-config")
+    
+last_change_position = running_config.find(last_applied_change)
 
-last_change_position = full_running_config.find(last_applied_change)
-
-# Extract the portion of the full running config after the last applied change
-running_config_after_last_change = full_running_config[last_change_position + len(last_applied_change):]
+# Extract the portion of the running config after the last applied change
+running_config_after_last_change = running_config[last_change_position + len(last_applied_change):]
 
 # Get the modification timestamp of the new_changes.txt file
 new_changes_file_path = "new_changes.txt"
@@ -89,6 +97,10 @@ with ConnectHandler(**{
             # Update the last applied change in the text file
             with open(last_change_file, "w") as f:
                 f.write(new_changes_normalized)
+            
+            # Save the updated running configuration to a file
+            with open("updated_running_config.txt", "w") as f:
+                f.write(running_config)
         else:
             print("No new configuration changes needed.")
     else:
