ba30d2ab9488f91a282002fe7a655beff1cafcb7
wbculver <wbculver@gmail.com>
2023-08-24 22:02:51 -0400

    Update pythonconfig-new-diff.py

--- pythonconfig-new-diff.py
+++ pythonconfig-new-diff.py
@@ -9,13 +9,13 @@ device_username = "admin"
 device_password = "admin"
 device_type = "cisco_ios"
 
-# Load configuration changes from YAML file
+# Load intended configuration changes from YAML file
 yaml_file = "config.yaml"
 with open(yaml_file) as f:
-    config_changes = yaml.safe_load(f)["config_changes"]
+    config_data = yaml.safe_load(f)
 
-# Lines of text to exclude from comparison
-excluded_lines = ["no logging console", "banner motd", "ntp server"]
+# Extract intended changes from the YAML file
+intended_changes = config_data.get("config_changes", [])
 
 # Connect to the device
 with ConnectHandler(**{
@@ -32,23 +32,23 @@ with ConnectHandler(**{
 
     # Normalize and calculate MD5 hash for running configuration
     running_config_lines = [line.strip() for line in running_config.split("\n") if line.strip() and not line.strip().startswith("!")]
-    running_config_normalized = "\n".join(line for line in running_config_lines if line not in excluded_lines)
+    running_config_normalized = "\n".join(running_config_lines)
     running_config_hash = hashlib.md5(running_config_normalized.encode()).hexdigest()
 
     # Calculate MD5 hash for intended configuration changes
-    intended_config = "\n".join(config_changes)
+    intended_config = "\n".join(intended_changes)
     intended_config_hash = hashlib.md5(intended_config.encode()).hexdigest()
 
     if intended_config_hash == running_config_hash:
         print("No configuration changes needed.")
     else:
-        print("Configuration differs, applying changes...")
+        print("Configuration differs in intended changes, applying changes...")
 
         config_commands = [
             "configure terminal"
         ]
         
-        config_commands.extend(config_changes)
+        config_commands.extend(intended_changes)
         
         config_commands.append("end")
 
